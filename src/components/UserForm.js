aa148f8472aff5a14b8eda5e48ea356388fa7b11
krisbratvold <kris.bratvold@gmail.com>
2021-07-09 14:32:53 -0700

    initial commit

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -7,6 +7,10 @@ const UserForm = (props) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+    const[firstNameError, setFirstNameError] = useState("");
+    const[lastNameError, setLastNameError] = useState("");
+    const[emailError, setEmailError] = useState("");
+    const[passwordError, setPasswordError] = useState("");
 
     const createUser = (e) => {
         e.preventDefault();
@@ -19,28 +23,93 @@ const UserForm = (props) => {
         console.log(newUser)
     };
 
+    const handleFirstName = (e) => {
+        setFirstName(e.target.value);
+        if(e.target.value.length <3){
+            setFirstNameError("Must be more than 2 characters");
+        }
+        else{
+            setFirstNameError("");
+        }
+    }
+
+    const handleLastName = (e) => {
+        setLastName(e.target.value);
+        if(e.target.value.length <3){
+            setLastNameError("Must be more than 2 characters");
+        }
+        else{
+            setLastNameError("");
+        }
+    }
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+        if(e.target.value.length <6){
+            setEmailError("Must be more than 5 characters");
+        }
+        else{
+            setEmailError("");
+        }
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+        if (password !== confirmPassword) {
+            setPasswordError('Passwords must match');
+        }
+        else if (e.target.value.length < 8) {
+            setPasswordError('Password must be 8 characters');
+        }
+        else{
+            setPasswordError("")
+        }
+    }
+
+
+
     return (
         <div>
             <form onSubmit={createUser}>
                 <div>
                     <label>First Name: </label>
-                    <input type="text" onChange={(e) => setFirstName(e.target.value)} value={firstName} />
+                    <input type="text" onChange={handleFirstName}/>
+                    {
+                        firstNameError ?
+                        <p style={{color:"red"}}>{firstNameError}</p>:
+                        ''
+                    }
                 </div>
                 <div>
                     <label>Last Name: </label>
-                    <input type="text" onChange={(e) => setLastName(e.target.value)} value={lastName} />
+                    <input type="text" onChange={handleLastName}/>
+                    {
+                        lastNameError ?
+                        <p style={{color:"red"}}>{lastNameError}</p>:
+                        ''
+                    }
                 </div>
                 <div>
                     <label>Email: </label>
-                    <input type="text" onChange={(e) => setEmail(e.target.value)} value={email} />
+                    <input type="text" onChange={ handleEmail} />
+                    {
+                        emailError ?
+                        <p style={{color:"red"}}>{emailError}</p>:
+                        ''
+                    }
                 </div>
                 <div>
                     <label>Password: </label>
-                    <input type="text" onChange={(e) => setPassword(e.target.value)} value={password} />
+                    <input type="text" onChange={handlePassword} />
+                    {
+                        passwordError ?
+                        <p style={{color:"red"}}>{passwordError}</p>:
+                        ''
+                    }
                 </div>
                 <div>
                     <label>Confirm Password: </label>
-                    <input type="text" onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword} />
+                    <input type="text" onChange={(e) => setConfirmPassword(e.target.value)} />
                 </div>
                 <button>Submit</button>
             </form>
